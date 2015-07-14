87d04b097aab9e4e32703c70410217063b2cb102
Tanya Marcol <martienzcolmenero.tv@gmail.com>
2015-07-14 12:31:51 -0700

    Added table to use countries and states (States are only available for MX & USA) plase run in order: 1) Run migrate command 2) Run seder classes:    a) CountriesSeeder    b) StatesSeeder

---
+++ database/migrations/2015_07_14_121001_create_states_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('country');
+            $table->string('code')->index();
+            $table->string('name');
+            $table->unique('code');
+        });
+        
+        Schema::table('states', function(Blueprint $table){
+            $table->foreign('country')->references('code')->on('countries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('states');
+    }
+}
