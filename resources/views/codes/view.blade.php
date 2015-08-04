ef1e3eb073e4422b97614f0b275509b3023d2ba5
Tanya Marcol <martienzcolmenero.tv@gmail.com>
2015-08-03 17:43:42 -0700

    Restyling code section

--- resources/views/codes/view.blade.php
+++ resources/views/codes/view.blade.php
@@ -3,19 +3,35 @@
 @section('content')
 
 <div class="row bg-gray-transparent">
-	<div class="codigo"></div>
-	{!! Form::open(array('url' => 'codes/check', 'id' => 'profile', 'name'=>'formulario')) !!}
-	<input type="hidden" name="_token" value="<?php echo csrf_token() ?>">
 	
-	<h2>
-		{{ Lang::get('code.promo') }}
-	</h2>
-	<div class="col-sm-10 col-sm-push-1 col-md-8 col-md-push-2 nopadding">				
-		<div class="inputs">
-			<?php echo Form::text('code', Input::get('code') ? Input::get('code') : @$code, array( 'required' => 'required'));?>
+	
+	<div class="col-sm-12 col-sm-pull-1 col-sm-offset-1 text-justify" style="font-size:24px;">
+		<div class="row">
+			<div class="col-xs-4 col-xs-push-8 col-sm-3 col-sm-push-9 col-md-2 col-md-push-10">
+				<a href="#" onClick="formulario.submit()" class="btn-blue-clear">{{ Lang::get('layout.continue') }}</a>
+			</div>
+			<div class="divider"></div>
+			<div class="col-sm-10 col-sm-push-1 col-md-8 col-md-push-2">
+				<div class="code"></div>
+			</div>
+		</div>
+
+		<div class="row text-center">
+			<div class="col-sm-12 nopadding spacing">
+			{{ Lang::get('code.promo') }}
+			</div>
 		</div>
 	</div>
-	{!! Form::close() !!}
+	<div class="col-sm-10 col-sm-push-1">
+		{!! Form::open(array('url' => 'codes/check', 'id' => 'profile', 'name'=>'formulario')) !!}
+
+		<input type="hidden" name="_token" value="<?php echo csrf_token() ?>">
+		<div class="inputs">
+			{!! Form::text('code', Input::get('code') ? Input::get('code') : @$code, array( 'required' => 'required')) !!}
+		</div>
+				
+		{!! Form::close() !!}
+	</div>
 </div>
 <div class="row bg-gray-transparent">
 	<div class="col-xs-8 col-xs-push-2 col-sm-4 col-sm-push-4">
