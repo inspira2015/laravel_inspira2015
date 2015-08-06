5e58c60ab63fdd3746e459c64ac4fbff395ef322
Daniel <daniel@ticktbox.com>
2015-08-05 19:14:09 -0700

    Exchange rate email

--- app/Http/Controllers/CronJobs/ExchangeRateUpdate.php
+++ app/Http/Controllers/CronJobs/ExchangeRateUpdate.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\CronJobs;
 use App\Http\Controllers\Controller;
-
+use Carbon; 
 use Request;
 use Redirect;
 use Response;
@@ -10,38 +10,56 @@ use Session;
 use Auth;
 use App\Model\Entity\ExchangeRateEntity;
 use App\Libraries\ApiExchangeRate\CurrentExchangeRate;
+use Mail;
 
 class ExchangeRateUpdate extends Controller 
 {
 	private $exchangeDao;
 	protected $auth;
 	private $currentExchangeRate;
-	
+	private $today;
+
 	
 	public function __construct( ExchangeRateEntity $exchange )
 	{
 		$this->middleware('guest');		
 		$this->exchangeDao =  $exchange;
 		$this->currentExchangeRate = new CurrentExchangeRate();
+		$this->today = Carbon::now();
 	}
 	
 
 	public function Currentrate()
 	{
 		
+		$dayRate = $this->exchangeDao->getByDate( $this->today->toDateString() , 'USDMXN' )->first();
+		$id = 0;
+		if ( !empty( $dayRate ) )
+		{
+			$id = $dayRate->id;
+		}
+
+		$currentRate = $this->currentExchangeRate->getExchangeRate();
+		$this->exchangeDao->exchangeArray(array( 'id' => $id ,
+												 'exchange_type' => 'USDMXN',
+												 'exchange_date' => $this->today->toDateString(),
+												 'exchange_rate' => $currentRate,
+										));
+		$this->exchangeDao->save();
+
+
+		$sent =Mail::send('emails.exchangerate', array( 'day' => $this->today->toFormattedDateString(),
+													    'hour' => $this->today->toTimeString(),
+													     'exchange' => $currentRate), function($message)
+			{
+				$today = Carbon::now();
+				$dayRate = $this->exchangeDao->getByDate( $today->toDateString() , 'USDMXN' )->first();
+				$currentRate = $dayRate->exchange_rate;
+		    	$message->to( 'danielgm78@msn.com', 'Daniel GOmez')->subject( 'Exchange USDMXN : ' . $currentRate );
+			});
+
 		echo $this->currentExchangeRate->getExchangeRate();
 
-
-
-
-
-
-
-
-		return Response::json(array(
-			'error' => false,
-			'data' => 'test'
-		), 200);
 
 	}
 }
---
+++ resources/views/emails/exchangerate.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+	<head>
+	  <meta charset="utf-8">
+	  <title>Activa tu cuenta</title>
+	  <meta name="description" content="The HTML5 Herald">
+	  <meta name="author" content="SitePoint">
+	</head>
+<body>
+	<div style="width:600px; margin:0 auto;">
+		<div style="@import url(http://inspiramexico.mx/resources/fonts/fonts.css?family=LondonBetween);">
+			<div>
+  				<div class="" style="width:100%; height:auto;"> 
+	 				<img  src="http://inspiramexico.mx/usuarios/images/header2.png" style="width:100%; height:auto;" />
+					<br>
+				</div>
+			</div>
+
+			<div align = "center" style="width:100%;  weight:60px; font-weight: bold; background-color: #E5E7E9;">
+				
+    			<hr width=80%;>
+
+				<br>
+				<div style="padding:10% 10% 10% 10%; margin:0 auto; width:60%; height:auto; border-style: solid; border-color:#d6dadd;  background-color: #ffffff; color:#616f7a; text-align:left; line-height: 19px;" align = "center">
+					TIPO DE CAMBIO:<br>
+					Dia: <?php echo $day; ?><br>
+					Hora: <?php echo $hour; ?><br>
+					Tipo de Cambio: <?php echo $exchange; ?><br>
+					
+				</div>
+			</div>	
+    
+			<div align = "center"> 
+				<a href="http://inspiramexico.mx/#openModal2">
+				<img  align = "center" src="http://inspiramexico.mx/usuarios/images/correo2_secc3.jpg" style="width:100%; heigth:auto"/>
+				</a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
