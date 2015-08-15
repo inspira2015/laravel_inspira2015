038ab8121b883211ff3c0fc5d40da08da1b42eb7
Tanya Marcol <martienzcolmenero.tv@gmail.com>
2015-08-15 12:25:48 -0700

    Password - rest layout

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,50 +1,55 @@
-@extends('app')
+@extends('layouts.basic', array('title' => Lang::get('auth.reset-password'), 'background' => '2.jpg' ) )
 
-@section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Reset Password</div>
-				<div class="panel-body">
-					@if (session('status'))
-						<div class="alert alert-success">
-							{{ session('status') }}
-						</div>
-					@endif
+@section('content')   
 
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
-
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Send Password Reset Link
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
+<div class="row" data-role="response">    
+    <div class="col-md-8 col-md-offset-2 bg-gray">
+	   
+	    <div class="row">
+		    
+		    <h2>{{ Lang::get('auth.reset-password') }}</h2>
+	    </div>
+	    
+	    <div class="row">
+		    <div class="divider"></div>
+	    </div>
+	    
+	    <div class="row">
+	    	<div class="col-md-10 col-md-push-1">
+		    	@if (session('status'))
+					<div class="alert alert-success">
+						{{ session('status') }}
+					</div>
+				@endif
+				
+				@include('errors.messages')
 			</div>
 		</div>
-	</div>
+	    <div class="row">
+			<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+				<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+				<div class="form-group">
+					<label for="email" >E-Mail</label>
+					<div class="input-group">
+						<input type="email"  class="form-control" name="email" value="{{ old('email') }}">
+					</div>
+				</div>
+				
+				<div class="divider"></div>
+
+				<div class="form-group">
+					<div class="col-md-5 col-md-offset-3 col-md-push-1">
+						<div class="row">
+							<br>
+							<button type="submit" class="btn-blue btn-medium">
+								{{ Lang::get('auth.reset-password') }}
+							</button>
+						</div>
+					</div>
+				</div>
+			</form>
+	    </div>
+    </div>
 </div>
 @endsection
