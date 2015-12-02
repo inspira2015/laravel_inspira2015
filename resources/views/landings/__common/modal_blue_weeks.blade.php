0adeb8e5d2d0acab4a7be1322f73b83d5008f902
Tanya Marcol <martienzcolmenero.tv@gmail.com>
2015-12-01 18:02:31 -0800

    Changes on promottion landing page

---
+++ resources/views/landings/__common/modal_blue_weeks.blade.php
@@ -0,0 +1,25 @@
+<div class="flat-windows">
+	<div class="modal fade" id="modal-semanas" tabindex="-1" role="dialog">
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-body text-center">
+		  	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	  		<div class="modal-light-blue-wrapper" data-role="response">
+  			<h2>SEMANAS AZULES</h2>
+  			</div>
+  			<div class="row">
+	  			<div class="col-xs-12">
+				<p>Existen per&iacute;odos donde hay gran demanda de hospedaje, como en época de vacaciones o 
+					temporada alta, lo cual hace que los precios aumenten. A este per&iacute;odo le llamamos SEMANAS AZULES.</p><br>
+				
+				<p>Para reservar en Semanas Azules es necesario usar tu certificado y adem&aacute;s pagar el 
+					monto que muestre la p&aacute;gina de reservaciones. </p><br>
+				
+				<p>Todos los resorts listados en nuestro sistema en semanas azules, 
+					cuentan con un precio 30% MENOR a cualquier otro precio listado en l&iacute;nea.</p>
+	  			</div>
+  			</div>
+	    </div>
+	  </div>
+	</div>
+</div>
--- resources/views/landings/promotions.blade.php
+++ resources/views/landings/promotions.blade.php
@@ -11,9 +11,6 @@
 					<h2>Viaja a los mejores lugares con inspira</h2>
 					Los resorts m&aacute;s lujosos alrededor del mundo, al precio m&aacute;s bajo<br>
 					<br>
-					<div class="row text-center">
-						<a href="{{ url('comprar-certificado') }}" class="btn-clear-white">COMPRAR CERTIFICADO</a>
-					</div>
 					<div class="row">
 					<a href="#" data-anchor="more-info" id="more-info">M&aacute;s info<br>
 	    				{!! HTML::image('images/more-info.png', 'M&aacute;s informaci&oacute;n') !!}
@@ -26,9 +23,7 @@
 					<h2>VIAJAR ES FÁCIL Y SEGURO CON INSPIRA</h2>
 					RESERVA F&Aacute;CILMENTE TODO LO NECESARIO PARA TU VIAJE DESDE NUESTRO SITIO<br>
 					<br>
-					<div class="row text-center">
-						<a href="{{ url('comprar-certificado') }}" class="btn-clear-white">COMPRAR CERTIFICADO</a>
-					</div>
+					
 					<div class="row">
 					<a href="#" data-anchor="more-info" id="more-info">M&aacute;s info<br>
 	    				{!! HTML::image('images/more-info.png', 'M&aacute;s informaci&oacute;n') !!}
@@ -41,9 +36,7 @@
 					<h2>VIAJA CON TODOS TUS AMIGOS CON INSPIRA</h2>
 					HOSPEDAJE DE HASTA 8 PERSONAS CON UN SOLO CERTIFICADO<br>
 					<br>
-					<div class="row text-center">
-						<a href="{{ url('comprar-certificado') }}" class="btn-clear-white">COMPRAR CERTIFICADO</a>
-					</div>
+					
 					<div class="row">
 					<a href="#" data-anchor="more-info" id="more-info">M&aacute;s info<br>
 	    				{!! HTML::image('images/more-info.png', 'M&aacute;s informaci&oacute;n') !!}
@@ -56,9 +49,7 @@
 					<h2>VIAJA POR TODO EL MUNDO CON INSPIRA</h2>
 					LOS RESORTS M&Aacute;S LUJOSOS DE 86 PA&Iacute;SES ALREDEDOR DEL MUNDO<br>
 					<br>
-					<div class="row text-center">
-						<a href="{{ url('comprar-certificado') }}" class="btn-clear-white">COMPRAR CERTIFICADO</a>
-					</div>
+					
 					<div class="row">
 					<a href="#" data-anchor="more-info" id="more-info">M&aacute;s info<br>
 	    				{!! HTML::image('images/more-info.png', 'M&aacute;s informaci&oacute;n') !!}
@@ -71,9 +62,36 @@
 					<h2>INSPIRA; UNA EXPERIENCIA INOLVIDABLE</h2>
 					EXPERIMENTA UN NUEVO NIVEL COMFORT Y LUJO QUE ENUNCA OLVIDAR&Aacute;S<br>
 					<br>
-					<div class="row text-center">
-						<a href="{{ url('comprar-certificado') }}" class="btn-clear-white">COMPRAR CERTIFICADO</a>
+					
+					<div class="row">
+					<a href="#" data-anchor="more-info" id="more-info">M&aacute;s info<br>
+	    				{!! HTML::image('images/more-info.png', 'M&aacute;s informaci&oacute;n') !!}
+    				</a>
 					</div>
+				</div>
+				<div class="swiper-slide" id="promotions-slide-6">
+					<h1><a href="{{ url('/') }}"><img src="images/logo_inspira.png"></a></h1><br>
+					
+					<h2>TUS VACACIONES DE ENSUE&Ntilde;O EN SOLO 3 PASOS:</h2>
+					<br>
+					<div class="row" id="steps">
+						<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2">
+							<div class="row">
+								<div class="col-xs-12 col-sm-4">
+									<div class="row">{!! HTML::image('images/register-check.png', 'Reg&iacute;strate') !!}</div>
+									<div class="row">1. REG&Iacute;STRATE</div>
+								</div>
+								<div class="col-xs-12 col-sm-4">
+									<div class="row">{!! HTML::image('images/cert.png', 'Comprar certificado') !!}</div>
+									<div class="row">2. COMPRA CERTIFICADO</div>
+								</div>
+								<div class="col-xs-12 col-sm-4">
+									<div class="row">{!! HTML::image('images/reserv.png', 'Reserva') !!}</div>
+									<div class="row">3. RESERVA</div>
+								</div>
+							</div>
+						</div>
+					</div>				
 					<div class="row">
 					<a href="#" data-anchor="more-info" id="more-info">M&aacute;s info<br>
 	    				{!! HTML::image('images/more-info.png', 'M&aacute;s informaci&oacute;n') !!}
@@ -89,17 +107,43 @@
 	<div class="row" id="map" data-id="more-info">
 		<div class="container">
 			<div class="row">
-				<p>Creemos que viajar es esencial para el bienestar, desarrollo, salud mental, f&iacute;sica y emocional de todo ser <br>
-					humano, por esta raz&oacute;n queremos que todas las personas, sin importar estado socioecon&oacute;mico, edad y<br>
-					limitaciones de tiempo ¡ VIAJEN !
-				</p>
+				<h2>¿ QU&Eacute; ES UN CERTIFICADO ?</h2>
+
+				<p>Un certificado equivale a hospedaje por una semana, para hasta 8 personas, en el resort de lujo de tu <br>
+				elección entre 86 países alrededor del mundo. Tú eliges la fecha de hospedaje, con un límite de tiempo de <br>
+				1 año para hacer la reservación desde la compra de tu certificado *</p>
 			</div>
 			<div class="row">
-				<h2>C&Oacute;MO FUNCIONA</h2>
-				<p>Reg&iacute;strate, compra tu certificado y reserva.</p><br>
-				<p>Conoce todos los resorts disponibles, dando clic en alguno de los destinos que se presentan abajo.
-				</p>
+				<div class="col-xs-12">
+					<h2>COMO FUNCIONA</h2>
+				</div>
+				<div class="col-xs-12 step">
+					<div class="row">
+						<div class="col-xs-12 col-sm-4">
+							<div class="row">{!! HTML::image('images/register-checkcolor.png', 'Reg&iacute;strate') !!}</div>
+							<div class="row">1. REG&Iacute;STRATE</div>
+						</div>
+						<div class="col-xs-12 col-sm-4">
+							<div class="row">{!! HTML::image('images/certcolor.png', 'Comprar certificado') !!}</div>
+							<div class="row">2. COMPRA CERTIFICADO</div>
+						</div>
+						<div class="col-xs-12 col-sm-4">
+							<div class="row">{!! HTML::image('images/reservcolor.png', 'Reserva') !!}</div>
+							<div class="row">3. RESERVA</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12">
+					Conoce todos los resorts disponibles, dando clic en alguno de los destinos que se presentan abajo.
+				</div>
+			</div>
 		</div>
+	</div>
+	<div class="row bg-light-blue">
+		<div class="container">
+			<div class="row">
+				* Solo la reservaci&oacute;n deber ser durante el periodo de un a&ntilde;o a partir de la compra, la fecha de tu vacaci&oacute;n no tiene limite de tiempo. Ejemplo: tu servicio debe ser durante el periodo del 2016, pero puedes llevar a cabo tu vacación en el 2017. <a href="#modal-semanas" data-toggle="modal" data-target="#modal-semanas"><span class="underline">ALTA DEMANDA O SEMANAS AZULES</span>&nbsp;&rsaquo;</a>
+			</div>
 		</div>
 	</div>
 	<div class="row text-center" id="destinations">
@@ -142,6 +186,16 @@
 			</ul>
 		</div>
 	</div>
+	<div class="row bg-medium-blue">
+		<div class="container">
+			<div class="row">
+				<p>Creemos que viajar es esencial para el bienestar, desarrollo, salud mental, f&iacute;sica y emocional de todo ser <br>
+					humano, por esta raz&oacute;n queremos que todas las personas, sin importar estado socioecon&oacute;mico, edad y<br>
+					limitaciones de tiempo ¡ VIAJEN !
+				</p>
+			</div>
+		</div>
+	</div>
 </div>
-
+@include('landings.__common.modal_blue_weeks')
 @endsection
